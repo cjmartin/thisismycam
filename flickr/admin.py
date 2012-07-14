2909db10ca12c2898a0c2e658159da58b5fc7f2e
Chris James Martin <cjmartin@gmail.com>
2012-07-13 20:13:05 -0700

    admin for accounts and flickr

---
+++ flickr/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from flickr.models import FlickrUser
+#from flickr.models import FlickrUserCamera
+
+#class FlickrUserCameraInline(admin.TabularInline):
+#    model = FlickrUserCamera
+#    extra = 0
+
+#class FlickrUserAdmin(admin.ModelAdmin):
+#    inlines = [
+#        FlickrUserCameraInline
+#    ]
+
+#admin.site.register(FlickrUser, FlickrUserAdmin)
+admin.site.register(FlickrUser)
+admin.site.register(FlickrUserCamera)
