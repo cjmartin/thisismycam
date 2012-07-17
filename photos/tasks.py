2a54c80d1085da488ac8e1f0e908fa7addc20e91
Chris James Martin <cjmartin@gmail.com>
2012-07-17 08:21:26 -0700

    debugging

--- photos/tasks.py
+++ photos/tasks.py
@@ -204,11 +204,13 @@ def process_flickr_photo(api_photo, nsid):
                     'count' : 0,
                 }
                 
-                try:
-                    camera = Camera.objects.create(**args)
-                    
-                except IntegrityError:
-                    raise process_flickr_photo.retry()
+                camera = Camera.objects.create(**args)
+                
+                # try:
+                #     camera = Camera.objects.create(**args)
+                #     
+                # except IntegrityError:
+                #     raise process_flickr_photo.retry()
                     
             photo.camera = camera
 
@@ -246,10 +248,10 @@ def process_flickr_photo(api_photo, nsid):
                         print "AWS image update for %s already scheculed, skipping." % (camera.name)
             
             # Update the FlickrUserCamera
-            if settings.DEBUG:
-                update_flickr_user_camera(nsid, camera.id, photo.photo_id)
-            else:
-                update_flickr_user_camera.delay(nsid, camera.id, photo.photo_id)
+            # if settings.DEBUG:
+            #     update_flickr_user_camera(nsid, camera.id, photo.photo_id)
+            # else:
+            #     update_flickr_user_camera.delay(nsid, camera.id, photo.photo_id)
 
 def clean_make(make):
     crap_words = [
