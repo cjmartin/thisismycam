e41d3bf2fa1852018f66b7d552574a2d7b81b27f
Chris Martin <cjmartin@gmail.com>
2012-07-17 17:03:38 +0200

    migration for FlickrUserCamera uniquenes

---
+++ flickr/migrations/0002_auto__add_unique_flickrusercamera_flickr_user_camera.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'FlickrUserCamera', fields ['flickr_user', 'camera']
+        db.create_unique('flickr_flickrusercamera', ['flickr_user_id', 'camera_id'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'FlickrUserCamera', fields ['flickr_user', 'camera']
+        db.delete_unique('flickr_flickrusercamera', ['flickr_user_id', 'camera_id'])
+
+
+    models = {
+        'cameras.camera': {
+            'Meta': {'object_name': 'Camera'},
+            'amazon_image_response': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'amazon_item_response': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'amazon_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'category': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['cameras.Category']", 'symmetrical': 'False'}),
+            'count': ('django.db.models.fields.IntegerField', [], {}),
+            'date_create': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'exif_make': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'exif_model': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'large_photo_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'make': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cameras.Make']", 'null': 'True', 'blank': 'True'}),
+            'medium_photo_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'small_photo_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'cameras.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'cameras.make': {
+            'Meta': {'object_name': 'Make'},
+            'count': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'flickr.flickruser': {
+            'Meta': {'object_name': 'FlickrUser'},
+            'cameras': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['cameras.Camera']", 'through': "orm['flickr.FlickrUserCamera']", 'symmetrical': 'False'}),
+            'count_photos_processed': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'date_create': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_photo_update': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'date_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'iconfarm': ('django.db.models.fields.IntegerField', [], {}),
+            'iconserver': ('django.db.models.fields.IntegerField', [], {}),
+            'nsid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'primary_key': 'True'}),
+            'path_alias': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'realname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'flickr.flickrusercamera': {
+            'Meta': {'unique_together': "(('flickr_user', 'camera'),)", 'object_name': 'FlickrUserCamera'},
+            'camera': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cameras.Camera']"}),
+            'comments_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'count_photos': ('django.db.models.fields.IntegerField', [], {}),
+            'date_first_taken': ('django.db.models.fields.DateTimeField', [], {}),
+            'date_first_upload': ('django.db.models.fields.DateTimeField', [], {}),
+            'date_last_taken': ('django.db.models.fields.DateTimeField', [], {}),
+            'date_last_upload': ('django.db.models.fields.DateTimeField', [], {}),
+            'date_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'faves_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'first_taken_id': ('django.db.models.fields.BigIntegerField', [], {}),
+            'first_upload_id': ('django.db.models.fields.BigIntegerField', [], {}),
+            'flickr_user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['flickr.FlickrUser']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_taken_id': ('django.db.models.fields.BigIntegerField', [], {}),
+            'last_upload_id': ('django.db.models.fields.BigIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['flickr']
