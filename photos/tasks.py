addba1b51b1c7a253881d29212689a1dcacbc43c
Chris James Martin <cjmartin@gmail.com>
2012-07-17 22:22:25 -0700

    more error catching around failed flickr queries

--- photos/tasks.py
+++ photos/tasks.py
@@ -47,7 +47,6 @@ def fetch_photos_for_flickr_user(nsid, page = 1, pages = 1):
 
     while page <= pages:
         print "Fetching page %s for %s" % (page, flickr_user.username)
-        
         try:
             photos_rsp = flickr.people.getPublicPhotos(user_id=flickr_user.nsid,extras="date_taken,date_upload,license,owner_name,media,path_alias,count_comments,count_faves,geo",page=page,format="json",nojsoncallback="true")
             json = simplejson.loads(photos_rsp)
@@ -97,170 +96,175 @@ def fetch_photos_for_flickr_user(nsid, page = 1, pages = 1):
     
 @task(ignore_result=True)
 def process_flickr_photo(api_photo, nsid):
-    exif_rsp = flickr.photos.getExif(photo_id=api_photo['id'],format="json",nojsoncallback="true")
-    exif = simplejson.loads(exif_rsp)
+    try:
+        exif_rsp = flickr.photos.getExif(photo_id=api_photo['id'],format="json",nojsoncallback="true")
+        exif = simplejson.loads(exif_rsp)
 
-    if exif and exif['stat'] == 'ok':
-        exif_camera = ""
-        raw_exif_make = ""
-        exif_make = ""
-        raw_exif_model = ""
-        exif_model = ""
-        exif_software = ""
+        if exif and exif['stat'] == 'ok':
+            exif_camera = ""
+            raw_exif_make = ""
+            exif_make = ""
+            raw_exif_model = ""
+            exif_model = ""
+            exif_software = ""
         
-        for tag in exif['photo']['exif'] :
-            if tag['label'] == "Make" :
-                raw_exif_make = tag['raw']['_content']
-                exif_make = clean_make(tag['raw']['_content'])
+            for tag in exif['photo']['exif'] :
+                if tag['label'] == "Make" :
+                    raw_exif_make = tag['raw']['_content']
+                    exif_make = clean_make(tag['raw']['_content'])
                 
-            if tag['label'] == "Model" :
-                raw_exif_model = tag['raw']['_content']
-                exif_model = clean_model(tag['raw']['_content'], exif_make)
+                if tag['label'] == "Model" :
+                    raw_exif_model = tag['raw']['_content']
+                    exif_model = clean_model(tag['raw']['_content'], exif_make)
                 
-            if tag['label'] == "Software" :
-                exif_software = tag['raw']['_content']
+                if tag['label'] == "Software" :
+                    exif_software = tag['raw']['_content']
                 
-        if exif['photo']['camera']:
-            exif_camera = exif['photo']['camera']
+            if exif['photo']['camera']:
+                exif_camera = exif['photo']['camera']
             
-        if exif_model:
-            naive = parse_datetime(api_photo['datetaken'])
-            api_date_taken = pytz.timezone("UTC").localize(naive)
+            if exif_model:
+                naive = parse_datetime(api_photo['datetaken'])
+                api_date_taken = pytz.timezone("UTC").localize(naive)
             
-            api_date_upload = datetime.utcfromtimestamp(float(api_photo['dateupload'])).replace(tzinfo=timezone.utc)
+                api_date_upload = datetime.utcfromtimestamp(float(api_photo['dateupload'])).replace(tzinfo=timezone.utc)
             
-            photo = Photo(
-                photo_id = api_photo['id'],
-                secret = api_photo['secret'],
-                server = api_photo['server'],
-                farm = api_photo['farm'],
-                title = api_photo['title'],
-                license = api_photo['license'],
-                media = api_photo['media'],
-                owner_nsid = api_photo['owner'],
-                owner_name = api_photo['ownername'],
-                path_alias = api_photo['pathalias'],
-                date_taken = api_date_taken,
-                date_upload = api_date_upload,
-                comments_count = api_photo['count_comments'],
-                faves_count = api_photo['count_faves'],
-            )
+                photo = Photo(
+                    photo_id = api_photo['id'],
+                    secret = api_photo['secret'],
+                    server = api_photo['server'],
+                    farm = api_photo['farm'],
+                    title = api_photo['title'],
+                    license = api_photo['license'],
+                    media = api_photo['media'],
+                    owner_nsid = api_photo['owner'],
+                    owner_name = api_photo['ownername'],
+                    path_alias = api_photo['pathalias'],
+                    date_taken = api_date_taken,
+                    date_upload = api_date_upload,
+                    comments_count = api_photo['count_comments'],
+                    faves_count = api_photo['count_faves'],
+                )
             
-            if api_photo['latitude'] or api_photo['longitude'] and api_photo['geo_is_public']:
-                photo.has_geo =  1
-                photo.latitude = api_photo['latitude']
-                photo.longitude = api_photo['longitude']
-                photo.accuracy = api_photo['accuracy']
-                photo.context = api_photo['context']
+                if api_photo['latitude'] or api_photo['longitude'] and api_photo['geo_is_public']:
+                    photo.has_geo =  1
+                    photo.latitude = api_photo['latitude']
+                    photo.longitude = api_photo['longitude']
+                    photo.accuracy = api_photo['accuracy']
+                    photo.context = api_photo['context']
                 
-                # try:
-                #     flickr_place = FlickrPlace.objects.get(place_id = api_photo['place_id'])
-                # except FlickrPlace.DoesNotExist:
-                #     flickr_place = FlickrPlace(
-                #         place_id = api_photo['place_id'],
-                #     )
-                #     flickr_place.save()
-                #     
-                #     if settings.DEBUG:
-                #         print "Fetching data for Flickr place %s" % flickr_place.place_id
-                #         process_flickr_place(flickr_place.place_id)
-                #     else:
-                #         process_flickr_place.delay(flickr_place.place_id)
-                #     
-                # photo.flickr_place = flickr_place
-            else:
-                photo.has_geo = 0
+                    # try:
+                    #     flickr_place = FlickrPlace.objects.get(place_id = api_photo['place_id'])
+                    # except FlickrPlace.DoesNotExist:
+                    #     flickr_place = FlickrPlace(
+                    #         place_id = api_photo['place_id'],
+                    #     )
+                    #     flickr_place.save()
+                    #     
+                    #     if settings.DEBUG:
+                    #         print "Fetching data for Flickr place %s" % flickr_place.place_id
+                    #         process_flickr_place(flickr_place.place_id)
+                    #     else:
+                    #         process_flickr_place.delay(flickr_place.place_id)
+                    #     
+                    # photo.flickr_place = flickr_place
+                else:
+                    photo.has_geo = 0
                 
-            camera_slug = slugify(exif_make + " " + exif_model)
+                camera_slug = slugify(exif_make + " " + exif_model)
             
-            try:
-                camera = Camera.objects.get(slug = camera_slug)
-                photo.camera_make = camera.make
+                try:
+                    camera = Camera.objects.get(slug = camera_slug)
+                    photo.camera_make = camera.make
                 
-            except Camera.DoesNotExist:
-                make = None
-                if exif_make:
-                    make_slug = slugify(exif_make)
-                    try:
-                        make = Make.objects.get(slug = make_slug)
-                        make.count = make.count + 1
+                except Camera.DoesNotExist:
+                    make = None
+                    if exif_make:
+                        make_slug = slugify(exif_make)
+                        try:
+                            make = Make.objects.get(slug = make_slug)
+                            make.count = make.count + 1
                         
-                    except Make.DoesNotExist:
-                        make = Make(
-                            slug = make_slug,
-                            name = exif_make,
-                            count = 1
-                        )
+                        except Make.DoesNotExist:
+                            make = Make(
+                                slug = make_slug,
+                                name = exif_make,
+                                count = 1
+                            )
                         
+                        try:
+                            make.save()
+                        except IntegrityError:
+                            raise process_flickr_photo.retry()
+                        
+                    photo.camera_make = make
+                
+                    if not exif_camera:
+                        if exif_make:
+                            exif_camera = exif_make + " " + exif_model
+                        else:
+                            exif_camera = exif_model
+                        
+                    args = {
+                        'slug' : camera_slug,
+                        'name' : exif_camera,
+                        'make' : make,
+                        'exif_make' : raw_exif_make,
+                        'model' : exif_model,
+                        'exif_model' : raw_exif_model,
+                        'count' : 0,
+                    }
+                
                     try:
-                        make.save()
+                        camera = Camera.objects.create(**args)
                     except IntegrityError:
                         raise process_flickr_photo.retry()
-                        
-                photo.camera_make = make
-                
-                if not exif_camera:
-                    if exif_make:
-                        exif_camera = exif_make + " " + exif_model
-                    else:
-                        exif_camera = exif_model
-                        
-                args = {
-                    'slug' : camera_slug,
-                    'name' : exif_camera,
-                    'make' : make,
-                    'exif_make' : raw_exif_make,
-                    'model' : exif_model,
-                    'exif_model' : raw_exif_model,
-                    'count' : 0,
-                }
-                
-                try:
-                    camera = Camera.objects.create(**args)
-                except IntegrityError:
-                    raise process_flickr_photo.retry()
                     
-            photo.camera = camera
+                photo.camera = camera
             
-            print "Saving photo %s for camera %s.\n" % (photo.photo_id, camera.name)    
-            photo.save()
+                print "Saving photo %s for camera %s.\n" % (photo.photo_id, camera.name)    
+                photo.save()
             
-            # In case we need to create cache keys
-            id_digest = md5(str(camera.id)).hexdigest()
+                # In case we need to create cache keys
+                id_digest = md5(str(camera.id)).hexdigest()
             
-            if not camera.amazon_item_response:
-                lock_id = "%s-lock-%s" % ("aws_update", id_digest)
-                acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
-                
-                if acquire_lock():
-                    if settings.DEBUG:
-                        print "Fetching aws info for %s.\n" % camera.name
-                        add_aws_item_to_camera(camera.id)
-                    else:
-                        add_aws_item_to_camera.delay(camera.id)
-                else:
-                    print "AWS item update for %s already scheculed, skipping." % (camera.name)
-                    
-            else:
-                if not camera.amazon_image_response:
-                    lock_id = "%s-lock-%s" % ("aws_image_update", id_digest)
+                if not camera.amazon_item_response:
+                    lock_id = "%s-lock-%s" % ("aws_update", id_digest)
                     acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
-                    
+                
                     if acquire_lock():
                         if settings.DEBUG:
-                            print "%s already has aws info.\n" % camera.name
-                            add_aws_photos_to_camera(camera.id)
+                            print "Fetching aws info for %s.\n" % camera.name
+                            add_aws_item_to_camera(camera.id)
                         else:
-                            add_aws_photos_to_camera.delay(camera.id)
+                            add_aws_item_to_camera.delay(camera.id)
                     else:
-                        print "AWS image update for %s already scheculed, skipping." % (camera.name)
+                        print "AWS item update for %s already scheculed, skipping." % (camera.name)
+                    
+                else:
+                    if not camera.amazon_image_response:
+                        lock_id = "%s-lock-%s" % ("aws_image_update", id_digest)
+                        acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
+                    
+                        if acquire_lock():
+                            if settings.DEBUG:
+                                print "%s already has aws info.\n" % camera.name
+                                add_aws_photos_to_camera(camera.id)
+                            else:
+                                add_aws_photos_to_camera.delay(camera.id)
+                        else:
+                            print "AWS image update for %s already scheculed, skipping." % (camera.name)
             
-            # Update the FlickrUserCamera
-            if settings.DEBUG:
-                update_flickr_user_camera(nsid, camera.id, photo.photo_id)
-            else:
-                update_flickr_user_camera.delay(nsid, camera.id, photo.photo_id)
-                
+                # Update the FlickrUserCamera
+                if settings.DEBUG:
+                    update_flickr_user_camera(nsid, camera.id, photo.photo_id)
+                else:
+                    update_flickr_user_camera.delay(nsid, camera.id, photo.photo_id)
+                    
+    except urllib2.URLError as e:
+        print "Problem talking to Flickr (%s) could not be reached due to %s, re-scheduling task." % (e.url, e.reason)
+        raise fetch_photos_for_flickr_user.retry()
+                    
 def clean_make(make):
     crap_words = [
     'PHOTO FILM CO\., LTD\.',
