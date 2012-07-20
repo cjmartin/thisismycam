604c5e39e25153c3b968384b99ca90b4e5b47122
Chris James Martin <cjmartin@gmail.com>
2012-07-20 00:47:53 -0700

    testing

--- flickr/tasks.py
+++ flickr/tasks.py
@@ -3,6 +3,8 @@ from django.utils import simplejson
 from django.db import IntegrityError
 from django.db.models import F
 
+import time
+
 from celery.task import task
 from flickr_api.api import flickr
 
@@ -21,7 +23,7 @@ def flickr_user_fetch_photos_complete(results, nsid):
     flickr_user = FlickrUser.objects.get(nsid = nsid)
 
     last_photo = Photo.objects.latest('date_upload')
-    flickr_user.date_last_photo_update = last_photo.date_upload
+    flickr_user.date_last_photo_update = time.mktime(last_photo.date_upload.timetuple())
     
     # logger.info("Processed %s photos for %s" % (len(photos_processed), flickr_user.username))
     # if flickr_user.count_photos_processed:
