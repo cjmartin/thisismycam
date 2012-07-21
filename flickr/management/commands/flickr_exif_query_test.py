870276a130def4b84b327284c831ed9b166c1995
Chris James Martin <cjmartin@gmail.com>
2012-07-20 18:45:25 -0700

    command to check a photo exif query

---
+++ flickr/management/commands/flickr_exif_query_test.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+from django.utils import simplejson
+from flickr_api.api import flickr
+
+class Command(BaseCommand):
+    help = 'Fetch photos for a flickr user.'
+    option_list = BaseCommand.option_list + (
+        make_option('--photo',
+        action='store',
+        dest='photo',
+        help='Photo id to fetch Exif for.'),
+    )
+
+    def handle(self, *args, **options):
+        exif_rsp = flickr.photos.getExif(photo_id=options.get('photo'),format="json",nojsoncallback="true")
+        exif = simplejson.loads(exif_rsp)
+        self.stdout.write(exif)
+        
+        self.stdout.write("All Done!\n")
