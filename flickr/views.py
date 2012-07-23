682d7c8f596dbd63f4865be7e4b50825b7f52737
Chris James Martin <cjmartin@gmail.com>
2012-07-23 10:18:49 -0700

    import re

--- flickr/views.py
+++ flickr/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.utils import simplejson
 
+import re
+
 from flickr.models import FlickrUser
 from cameras.models import Camera
 from photos.models import Photo
