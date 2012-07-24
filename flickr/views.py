16cf6e2a3a727dd8809d5f79bb3f492e8eccd85c
Chris James Martin <cjmartin@gmail.com>
2012-07-23 17:50:44 -0700

    user/camera view

--- flickr/views.py
+++ flickr/views.py
@@ -20,24 +20,7 @@ def user(request, user_slug):
         user = request.user.get_profile()
         
         user_cameras = flickr_user.flickrusercamera_set.order_by('-date_last_taken', '-count_photos')
-        
-        cameras_and_photos = []
-        for user_camera in user_cameras:
-            if user_camera.camera.amazon_image_response:
-                user_camera.camera.amazon_image_response = simplejson.loads(user_camera.camera.amazon_image_response)
-                
-            if user_camera.camera.amazon_item_response:
-                user_camera.camera.amazon_item_response = simplejson.loads(user_camera.camera.amazon_item_response)
-                if not user_camera.camera.amazon_url:
-                    # Clean up and add the item url
-                    url = urllib2.unquote(user_camera.camera.amazon_item_response['DetailPageURL'])
-                    split_url = url.split('?')
-                    pretty_url = split_url[0] + "?tag=" + settings.AWS_ASSOCIATE_TAG
-                    user_camera.camera.amazon_url = pretty_url
-                    
-            photos = Photo.objects.filter(camera = user_camera.camera, owner_nsid = flickr_user.nsid).order_by('-date_taken')[:6]
-            cameras_and_photos.append({'user_camera':user_camera, 'photos':photos})
-        
+        cameras_and_photos = load_photos_for_cameras(user_cameras)
         primary_camera = user_cameras[0]
         
         photos = Photo.objects.filter(camera = primary_camera.camera, owner_nsid = flickr_user.nsid).order_by('-date_taken')[:18]
@@ -51,9 +34,50 @@ def user(request, user_slug):
         }
     
         return render_to_response('cameras/user_index.html', data)
+        
+def user_camera(request, user_slug, camera_slug):
+    user = request.user.get_profile()
+    flickr_user = get_user_by_slug(user_slug)
+    camera = get_object_or_404(Camera, slug=camera_slug)
+    
+    user_cameras = flickr_user.flickrusercamera_set.order_by('-date_last_taken', '-count_photos')
+    cameras_and_photos = load_photos_for_cameras(user_cameras)
+    
+    primary_camera = flickr_user.cameras.get(camera = camera)
+    first_taken_photo = Photo.objects.get(photo_id = primary_camera.first_taken_id)
+    
+    photos = Photo.objects.filter(camera = primary_camera.camera, owner_nsid = flickr_user.nsid).order_by('-date_taken')[:18]
+    
+    data = {
+        'user': user,
+        'flickr_user': flickr_user,
+        'user_cameras': cameras_and_photos,
+        'primary_camera': primary_camera,
+        'photos': photos,
+    }
 
-# def user_camera
+    return render_to_response('cameras/user_index.html', data)
+    
+def load_photos_for_cameras(cameras):
+    cameras_and_photos = []
+    for user_camera in user_cameras:
+        if user_camera.camera.amazon_image_response:
+            user_camera.camera.amazon_image_response = simplejson.loads(user_camera.camera.amazon_image_response)
+            
+        if user_camera.camera.amazon_item_response:
+            user_camera.camera.amazon_item_response = simplejson.loads(user_camera.camera.amazon_item_response)
+            if not user_camera.camera.amazon_url:
+                # Clean up and add the item url
+                url = urllib2.unquote(user_camera.camera.amazon_item_response['DetailPageURL'])
+                split_url = url.split('?')
+                pretty_url = split_url[0] + "?tag=" + settings.AWS_ASSOCIATE_TAG
+                user_camera.camera.amazon_url = pretty_url
                 
+        photos = Photo.objects.filter(camera = user_camera.camera, owner_nsid = flickr_user.nsid).order_by('-date_taken')[:6]
+        cameras_and_photos.append({'user_camera':user_camera, 'photos':photos})
+        
+    return cameras_and_photos:
+    
 def get_user_by_slug(user_slug):
     # NSID
     if re.search('(@N\d+)$', user_slug):
