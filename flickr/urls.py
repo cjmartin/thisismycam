8a27478b069310d259caee7e558376128f5d610c
Chris James Martin <cjmartin@gmail.com>
2012-07-23 18:42:05 -0700

    try named args

--- flickr/urls.py
+++ flickr/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'flickr.views.user'),
-    url(r'^cameras/([\w-]+)', 'flickr.views.user_camera', name='flickr-user-camera'),
+    url(r'^cameras/(?P<camera_slug>[\w-]+)', 'flickr.views.user_camera', name='flickr-user-camera'),
 )
