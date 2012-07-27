174246f818dac8498de1927f5b1a225a08fa9148
Chris Martin <cj@cjmart.in>
2012-07-27 14:25:25 -0700

    adding pushy.js

---
+++ pushy/pushy.js
@@ -0,0 +1,36 @@
+var crypto = require('crypto');
+
+var pushy_secret = "super secret secret";
+var pushy_salt = "super salty salt";
+
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+
+app.listen(8080);
+
+app.get('/', function (req, res) {
+    res.send("I'm a teapot", 418);
+});
+
+app.post('/', function (req, res) {
+
+    if (req.body.secret && req.body.secret == pushy_secret) {
+        
+        var user_id = req.body.user_id;
+        
+        var shasum = crypto.createHash('sha1');
+        shasum.update(user_id + pushy_salt);
+        
+        var channel = user_id + '_' + shasum.digest('hex');
+        
+        io.sockets.emit(channel, { msg: req.body.message });
+
+        res.send('OK');
+    
+    } else {
+        
+        res.send("I'm a teapot", 418);
+        
+    }
+
+});
