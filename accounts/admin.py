62a6df6f5061241fe53c1563f6ca842bd6706730
Chris Martin <cj@cjmart.in>
2012-07-30 21:54:47 -0700

    remove flickrcontact from accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
 
 from accounts.models import UserProfile
-from accounts.models import FlickrContact
-    
-class FlickrContactInline(admin.TabularInline):
-    model = FlickrContact
-    extra = 0
-    
-class UserProfileAdmin(admin.ModelAdmin):
-    inlines = [
-        FlickrContactInline
-    ]
 
-admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(FlickrContact)
+admin.site.register(UserProfile)
