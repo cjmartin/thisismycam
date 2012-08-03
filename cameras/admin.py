154609d1e43fbdd5e84c0dada9b519504f0078fb
Chris Martin <cj@cjmart.in>
2012-08-02 23:42:33 -0700

    attempt to add camera images to admin

--- cameras/admin.py
+++ cameras/admin.py
@@ -4,6 +4,19 @@ from cameras.models import Camera
 from cameras.models import Make
 from cameras.models import Category
 
-admin.site.register(Camera)
+class CameraAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    
+    def change_view(self, request, object_id, extra_context=None):
+        camera = Camera.objects.get(id = int(object_id))
+        aws_items = get_aws_info(camera)
+                
+        extra_context = {
+            'camera': camera,
+            'aws_items': aws_items,
+        }
+        return super(CameraAdmin, self).change_view(request, object_id, extra_context=extra_context)
+
+admin.site.register(Camera, CameraAdmin)
 admin.site.register(Make)
 admin.site.register(Category)
