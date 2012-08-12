38eb671282dd5ef0fbacd0de369e6f4e7e06ad27
Chris Martin <cj@cjmart.in>
2012-08-12 13:55:44 -0700

    updates to setup page and flickr fetch user photos script

--- flickr/management/commands/flickr_fetch_user_photos.py
+++ flickr/management/commands/flickr_fetch_user_photos.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 
+from django.utils import simplejson
+from flickr_api.api import flickr
+
 from flickr.models import FlickrUser
 
 from photos.tasks import fetch_photos_for_flickr_user
@@ -17,6 +20,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = FlickrUser.objects.get(nsid=options.get('user'))
+        
+        if not user.count_photos:
+            rsp = flickr.people.getInfo(user_id=user.nsid,format="json",nojsoncallback="true")
+            json = simplejson.loads(rsp)
+
+            if json and json['stat'] == "ok":
+                api_user = json['person']
+                
+                user.count_photos = api_user['photos']['count']['_content']
+                user.save()                
+            
         fetch_photos_for_flickr_user.delay(None, user.nsid)
         
         self.stdout.write("All Done!\n")
