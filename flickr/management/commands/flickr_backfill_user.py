ffcbded411156e0b27cd8007a6d8281496f4cd53
Chris Martin <cj@cjmart.in>
2012-08-17 14:15:06 -0400

    backfill for count_cameras

---
+++ flickr/management/commands/flickr_backfill_user.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+from flickr.models import FlickrUser
+
+class Command(BaseCommand):
+    help = 'Backfill flickr users'
+
+    def handle(self, *args, **options):
+        flickr_users = FlickrUser.objects.all()
+        users_to_update = []
+        
+        for flickr_user in flickr_users:
+            
+            flickr_user.count_cameras = flickr_user.cameras.count()
+            flickr_user.save()
+            
+            # if flickr_user.date_last_photo_update:            
+            #     flickr_user.initial_fetch_completed = True
+            #         
+            #     self.stdout.write("Setting initial fetch completed for %s\n" % (flickr_user.username))
+            #     flickr_user.save()
+            # else:
+            #     users_to_update.append(flickr_user)
+            #     self.stdout.write("Scheduling %s for an update\n" % (flickr_user.username))
+        
+        self.stdout.write("All Done!\n")
