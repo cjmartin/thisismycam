0d6dfbe90eca4be60488aea136e2afdebb987711
Chris Martin <cj@cjmart.in>
2012-08-17 13:50:01 -0400

    backfill for initial fetch status

--- flickr/management/commands/flickr_update_user_current_camera.py
+++ flickr/management/commands/flickr_update_user_current_camera.py
@@ -5,24 +5,20 @@ from optparse import make_option
 from flickr.models import FlickrUser
 
 class Command(BaseCommand):
-    help = 'Clears all cameras and photos stored for a Flickr user.'
-    option_list = BaseCommand.option_list + (
-        make_option('--user',
-        action='store',
-        dest='user',
-        help='User (flickr) update.'),
-    )
+    help = 'Backfill for initial fetch status'
 
     def handle(self, *args, **options):
         flickr_users = FlickrUser.objects.all()
-
-        for flickr_user in flickr_users:
-            if flickr_user.cameras.count():            
-                flickr_user.current_camera = flickr_user.calculate_current_camera()
+        users_to_update = []
         
-                self.stdout.write("Setting current camera for %s to %s\n" % (flickr_user.username, flickr_user.current_camera.camera.name))
+        for flickr_user in flickr_users:
+            if flickr_user.date_last_photo_update:            
+                flickr_user.initial_fetch_completed = True
+        
+                self.stdout.write("Setting initial fetch completed for %s\n" % (flickr_user.username))
                 flickr_user.save()
             else:
-                self.stdout.write("%s doesn't have any cameras\n" % (flickr_user.username))
+                users_to_update.append(flickr_user)
+                self.stdout.write("Scheduling %s for an update\n" % (flickr_user.username))
         
         self.stdout.write("All Done!\n")
